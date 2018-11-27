ac478d1f420e62eb667fe1890a8c799261515ea5
be-doing <1782904648@qq.com>
2018-11-27 22:03:34 +0800

    待修改

--- 顺序表的接口实现/顺序表的接口实现/main.c
+++ 顺序表的接口实现/顺序表的接口实现/main.c
@@ -1,11 +1,13 @@
 #include "SeqList.h"
-
+#include<stdlib.h>
  void main()
  {
 	Sql sql_main;
 	sql_init(&sql_main,10);
 	sql_add_front(&sql_main, 1);
-	sql_add_front(&sql_main, 1);
-	sql_add_front(&sql_main, 1);
+	sql_add_front(&sql_main, 2);
+	sql_add_front(&sql_main, 3);
 	print_arr(&sql_main);
+
+	system("pause");
  }
--- 顺序表的接口实现/顺序表的接口实现/顺序表的接口实现.c
+++ 顺序表的接口实现/顺序表的接口实现/顺序表的接口实现.c
@@ -1,12 +1,13 @@
 #include "SeqList.h"
 
 #include<stdio.h>
+#include<stdlib.h>
 
-Sql seqlist;
+//Sql sql;
 
 void sql_init(Sql* sl, size_t maxnum) 
 {
-	sl->arr = 0;
+	sl->arr = (int*)malloc(sizeof(int)*maxnum);
 	sl->maxNum = maxnum;
 	sl->size = 0;
 }
@@ -19,11 +20,21 @@ void sql_destroy(Sql* sl)
 
 void print_arr(Sql* sl)
 {
-	for (size_t i = 0; i < sl->size-1; i++)
+	for (size_t i = 0; i < sl->size; i++)
 	{
 		printf("%d ",sl->arr[i]);
 	}
 	printf("\n");
+}
+
+//扩容接口
+void check_maxNum(Sql* sl) 
+{
+	if (sl->size==sl->maxNum)
+	{
+		printf("正在扩容……\n");
+		sl->maxNum *= 2;
+	}
 }
 
 //增接口
@@ -43,3 +54,21 @@ void sql_add_end(Sql* sl, int num)
 	sl->arr[sl->size] = num;
 	++sl->size;
 }
+
+//删接口
+	//头删
+void sql_del_front(Sql* sl)
+{
+	for (size_t del = 0; del < sl->size-1; del++)
+	{
+		sl->arr[del] = sl->arr[del + 1];
+	}
+		--sl->size;
+}
+	//尾删
+void sql_del_front(Sql* sl)
+{
+	sl->arr[sl->size - 1] = sl->arr[sl->size];
+	--sl->size;
+}
+
