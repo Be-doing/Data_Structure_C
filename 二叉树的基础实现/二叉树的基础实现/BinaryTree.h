c552e9dfc77ccf6c4a144120e491b09cb7a3bbc7
be-doing <1782904648@qq.com>
2018-12-19 23:06:35 +0800

    待续

--- 二叉树的基础实现/二叉树的基础实现/BinaryTree.h
+++ 二叉树的基础实现/二叉树的基础实现/BinaryTree.h
@@ -8,7 +8,7 @@ typedef struct BinaryTree
 }BTnode;
 
 //构建二叉树
-BTnode* BinaryTreeCreate(BTDataType* arr, int size, BTDataType* root);
+BTnode* BinaryTreeCreate(BTDataType* arr, int size, int sign);
 
 //二叉树的销毁
 void BinaryTreeDestroy(BTnode* root);
@@ -20,6 +20,8 @@ int BinaryTreeLeafSize(BTnode* root);
 BTnode* BinaryTreeFindNode(BTnode* root, BTDataType data);
 
 //前中后遍历
-
+void BinaryTreePrevorder(BTnode* root);
+void BinaryTreeInorder(BTnode* root);
+void BinaryTreePostorder(BTnode* root);
 //判断二叉树是否是完全二叉树
 int BinaryTreeComplete(BTnode* root);
--- 二叉树的基础实现/二叉树的基础实现/二叉树的基础实现.C
+++ 二叉树的基础实现/二叉树的基础实现/二叉树的基础实现.C
@@ -1,16 +1,20 @@
 #include"BinaryTree.h"
 #include<assert.h>
 //构建二叉树
-BTnode* BinaryTreeCreate(BTDataType* arr, int size, BTnode* root)
+BTnode* BinaryTreeCreate(BTDataType* arr, int size, int sign)
 {
 	assert(arr);
-	assert(root);
-	if (root == NULL)
+	if (arr[sign] == '#')
 	{
 		return;
 	}
 	BTnode* node = (BTnode*)malloc(sizeof(BTnode));
-	
+	node->data = arr[sign];
+	++sign;
+	node->left = BinaryTreeCreate(arr, size, sign);
+	++sign;
+	node->right = BinaryTreeCreate(arr, size, sign);
+	return node;
 }
 
 //二叉树的销毁
@@ -23,6 +27,36 @@ int BinaryTreeLeafSize(BTnode* root);
 BTnode* BinaryTreeFindNode(BTnode* root, BTDataType data);
 
 //前中后遍历
+void BinaryTreePrevorder(BTnode* root)
+{
+	if (root == NULL)
+	{
+		return;
+	}
+	printf("%d ", root->data);
+	BinaryTreePrevorder(root->left);
+	BinaryTreePrevorder(root->right);
+}
+void BinaryTreeInorder(BTnode* root)
+{
+	if (root == NULL)
+	{
+		return;
+	}
 
+	BinaryTreeInorder(root->left);
+	printf("%d ", root->data);
+	BinaryTreeInorder(root->right);
+}
+void BinaryTreePostorder(BTnode* root)
+{
+	if (root == NULL)
+	{
+		return;
+	}
+	BinaryTreePostorder(root->left);
+	BinaryTreePostorder(root->right);
+	printf("%d ", root->data);
+}
 //判断二叉树是否是完全二叉树
 int BinaryTreeComplete(BTnode* root);
