0c06490b182b406021905043cc27f1812837bf1f
be-doing <1782904648@qq.com>
2018-12-24 23:39:32 +0800

    待续

--- 排序实现/排序实现/排序实现.C
+++ 排序实现/排序实现/排序实现.C
@@ -50,6 +50,23 @@ int* insert_sort(int* arr, int size)
 	}
 
 }
+//—°‘Ò≈≈–Ú
+int* choice_sort(int *arr, int size)
+{
+	int i, j, temp;
+	for (i = 0; i < size - 1; i++)
+	{
+		for (j = i + 1; j < size; j++)
+		{
+			if (arr[i] > arr[j])
+			{
+				temp = arr[i];
+				arr[i] = arr[j];
+				arr[j] = temp;
+			}
+		}
+	}
+}
 main()
 {
 	int arr[] = { 1,2,5,7,6,3,4,8,4 };
