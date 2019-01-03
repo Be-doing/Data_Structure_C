8ecd19be654fa203855f6be95d7fe090f5bd2c79
be-doing <1782904648@qq.com>
2019-01-03 23:07:26 +0800

    待续

--- 排序实现/排序实现/排序实现.C
+++ 排序实现/排序实现/排序实现.C
@@ -93,6 +93,39 @@ void quick_sort(int s[], int l, int r)
 		quick_sort(s, i + 1, r);
 	}
 }
+void mergeAdd(int arr[], int left, int mid, int right, int *temp) {
+		int i = left;
+	int j = mid + 1;
+	int k = left;//临时下标
+	while (i <= mid && j <= right) {
+		if (arr[i] < arr[j]) {
+			temp[k++] = arr[i++];
+		}
+		else {
+			temp[k++] = arr[j++];
+		}
+	}
+	while (i <= mid) {
+		temp[k++] = arr[i++];
+	}
+	while (j <= right) {
+		temp[k++] = arr[j++];
+	}
+	//把temp中的内容拷给arr数组中
+	//进行归并的时候，处理的区间是arr[left,right),对应的会把
+	//这部分区间的数组填到tmp[left,right)区间上
+	memcpy(arr + left, temp + left, sizeof(int)*(right - left + 1));
+}
+void mergeSort(int arr[], int left, int right, int *temp) {
+	int mid = 0;
+	if (left < right) {
+		mid = left + (right - left) / 2;
+		mergeSort(arr, left, mid, temp);
+		mergeSort(arr, mid + 1, right, temp);
+		mergeAdd(arr, left, mid, right, temp);
+	}
+
+}
 main()
 {
 	int arr[] = { 1,2,5,7,6,3,4,8,4 };
