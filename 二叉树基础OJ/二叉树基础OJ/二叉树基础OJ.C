c974ea29103b372496e41a485d82530e90ece62a
be-doing <1782904648@qq.com>
2019-01-08 21:32:54 +0800

    待续

--- 二叉树基础OJ/二叉树基础OJ/二叉树基础OJ.C
+++ 二叉树基础OJ/二叉树基础OJ/二叉树基础OJ.C
@@ -55,7 +55,7 @@ int judge_same(Btree* root1, Btree* root2)
 		judge_same(root1->right, root2->right);
 }
 //对称二叉树
-int judge_sym(Btree* root1, Btree* root2)
+int judge_sam(Btree* root1, Btree* root2)
 {
 	if ((root1 == NULL) && (root2 == NULL))
 	{
@@ -68,4 +68,51 @@ int judge_sym(Btree* root1, Btree* root2)
 	return (root1->data == root2->data) &&
 		judge_sym(root1->left, root2->right) &&
 		judge_sym(root1->right, root2->left);
+}
+//二叉树的高度
+int GetHeight(Btree* root)
+{
+	if (root == NULL)
+	{
+		return 0;
+	}
+	int l = GetHeight(root->left);
+	int r = GetHeight(root->right);
+	if (l > r)
+	{
+		return l + 1;
+	}
+	else
+	{
+		return r + 1;
+	}
+}
+//平衡二叉树
+//一个二叉树每个节点 的左右两个子树的高度差的绝对值不超过1。
+
+int isBalanced(Btree* root) {
+		if (root == NULL )
+		{
+			return 1;
+		}
+		int l = GetHeight(root->left);
+		int r = GetHeight(root->right);
+		if ( -1 <=(l - r) <= 1)
+		{
+			return 1;
+		}
+		return 0;
+}
+
+//建二叉树
+//接口结构体
+typedef struct MyStruct
+{
+	Btree* node;
+	int used;
+}MyStruct;
+MyStruct CreateTree(char arr[], int size)
+{
+	Btree* tree = (Btree*)malloc(sizeof(Btree));
+
 }
