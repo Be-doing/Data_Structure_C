18882f3d51f05aeacf7fba5cfe3fd7f3c964b27d
be-doing <1782904648@qq.com>
2019-01-23 22:31:14 +0800

    待续

--- 排序实现/排序实现/排序实现.C
+++ 排序实现/排序实现/排序实现.C
@@ -155,31 +155,31 @@ void HeapSort(int arr[], int size)
 		Adjust_down(arr, size-temp,0);
 	}
 }
-////快速排序
-//void quick_sort(int s[], int l, int r)
-//{
-//	if (l < r)
-//	{
-//		int i = l, j = r, x = s[l];
-//		while (i < j)
-//		{
-//			while (i < j && s[j] >= x)//从右到左找到第一个小于x的数  
-//				j--;
-//			if (i < j)
-//				s[i++] = s[j];
-//
-//			while (i < j && s[i] <= x)//从左往右找到第一个大于x的数  
-//				i++;
-//			if (i < j)
-//				s[j--] = s[i];
-//
-//		}
-//
-//		s[i] = x;//i = j的时候，将x填入中间位置  
-//		quick_sort(s, l, i - 1);//递归调用 
-//		quick_sort(s, i + 1, r);
-//	}
-//}
+//快速排序
+void Quick_Sort(int s[], int l, int r)
+{
+	if (l < r)
+	{
+		int i = l, j = r, x = s[l];
+		while (i < j)
+		{
+			while (i < j && s[j] >= x)//从右到左找到第一个小于x的数  
+				j--;
+			if (i < j)
+				s[i++] = s[j];
+
+			while (i < j && s[i] <= x)//从左往右找到第一个大于x的数  
+				i++;
+			if (i < j)
+				s[j--] = s[i];
+
+		}
+
+		s[i] = x;//i = j的时候，将x填入中间位置  
+		Quick_Sort(s, l, i - 1);//递归调用 
+		Quick_Sort(s, i + 1, r);
+	}
+}
 //void mergeAdd(int arr[], int left, int mid, int right, int *temp) {
 //		int i = left;
 //	int j = mid + 1;
